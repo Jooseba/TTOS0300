0386d2f47401a38b6e25b1555a00b28bc7d7adf9
Joose Seppälä <joose.seppala@gmail.com>
2019-03-22 14:56:01 +0200

    Password UI done

---
+++ WpfApp1/WpfPwdCheck/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPwdCheck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PsbPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            //show count of different kind of marks
+            int cntTotal = psbPassword.Password.Length;
+            txtTotal.Text = "Merkkejä: " + cntTotal;
+
+            //change label color and text
+            string msg;
+            Color color;
+            if (cntTotal > 15)
+            {
+                msg = "Salasana on vahva";
+                color = Colors.Green;
+            }
+            else if (cntTotal > 8)
+            {
+                msg = "Salasana on välttävä";
+                color = Colors.Yellow;
+            }
+            else
+            {
+                msg = "Salasana on huono";
+                color = Colors.Red;
+            }
+
+            lblMessage.Content = msg;
+            SolidColorBrush brush = new SolidColorBrush(color);
+            lblMessage.Background = brush;
+        }
+    }
+}
